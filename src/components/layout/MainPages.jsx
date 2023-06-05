31696d0918a2cbfb506dd96c7272ffc926bb4b65
NOWORRYGNN <rohmanal83@gmail.com>
2023-06-05 23:09:33 +0700

    News 0.1

--- src/components/layout/MainPages.jsx
+++ src/components/layout/MainPages.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Section1banner from './Section1banner'
 import Navbar from './navbar'
 
 const mainPages = () => {
   return (
     <div>
         <Navbar />
+        <Section1banner />
     </div>
   )
 }
---
+++ src/components/layout/Section1banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section1banner = () => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>Banner</div>
+        <div className='col'>SideContent</div>
+      </div>
+    </div>
+  )
+}
+
+export default Section1banner
