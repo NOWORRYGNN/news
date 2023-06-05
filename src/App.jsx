b313019954b78316ef084c9c09157aa4adeeead9
NOWORRYGNN <rohmanal83@gmail.com>
2023-06-05 23:02:36 +0700

    News 0.1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import './App.css';
+import MainPages from './components/layout/MainPages';
+
+function App() {
+  return (
+    <>
+      <MainPages />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/layout/navbar.jsx
@@ -0,0 +1,44 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import brandImage from '../../assets/react.svg';
+
+function navbar() {
+  return (
+    <>
+      {['md'].map((expand) => (
+        <Navbar key={expand} bg="white" expand={expand} className="mb-3 p-4">
+          <Container fluid>
+            <Navbar.Brand href="#">
+                <img src={brandImage} alt='brandImage' />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3 gap-3" style={{fontSize:15}}>
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">New</Nav.Link>
+                  <Nav.Link href='#action3'>Popular</Nav.Link>
+                  <Nav.Link href='#action4'>Trending</Nav.Link>
+                  <Nav.Link href='#action5'>Categories</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default navbar;
