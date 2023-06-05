267748c8f224a06eaca24bfc17f0b3bc7a8b13f2
NOWORRYGNN <rohmanal83@gmail.com>
2023-06-05 23:15:07 +0700

    News 0.2

--- src/components/layout/Section1banner.jsx
+++ src/components/layout/Section1banner.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Section2sidecontent from './Section2sidecontent'
 
 const Section1banner = () => {
   return (
     <div className='container'>
       <div className='row'>
         <div className='col'>Banner</div>
-        <div className='col'>SideContent</div>
+        <div className='col'>
+          <Section2sidecontent />
+        </div>
       </div>
     </div>
   )
---
+++ src/components/layout/Section2sidecontent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Section2sidecontent = () => {
+  return (
+    <div>Section2sidecontent</div>
+  )
+}
+
+export default Section2sidecontent
